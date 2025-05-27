60dd4e761e5e4c541cfb850c2a7f3897ea2e5a88
fas-a <abulkhoirfaris0@gmail.com>
2025-05-27 08:25:45 +0700

    login page locator

--- maven/src/test/java/io/cucumber/skeleton/locators/LoginPageLocators.java
+++ maven/src/test/java/io/cucumber/skeleton/locators/LoginPageLocators.java
@@ -8,4 +8,14 @@ public class LoginPageLocators {
     //@FindBy(xpath = "//*[@id=\"app\"]/div/div/div/div/div[1]/header/div/div/h3")
     //public WebElement pageTitle;
 
+    @FindBy(xpath = "/html/head/title")
+    public WebElement pageTitle;
+    @FindBy(name = "username")
+    public WebElement usernameInput;
+    @FindBy(name = "password")
+    public WebElement passwordInput;
+    @FindBy(xpath = "/html/body/div[4]/div/div[2]/div/div/form/button")
+    public WebElement loginButton;
+    @FindBy(xpath = "/html/body/div[2]/div/div[2]/div/div/div[2]/div/p")
+    public WebElement errorMessage;
 }
